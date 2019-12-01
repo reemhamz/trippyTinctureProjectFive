2b3001749aac43c34db29a1e2daf4ef46709b62b
Reem H <reemhamoui92@gmail.com>
2019-12-01 13:41:21 -0500

    adding image path to random pokemon

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
       latinPhrase2: "this is a placeholder in english",
       latinPhrase3: "another placeholder",
       latinPhrase4: "another another",
-      latinPhrase5: "wow the 5th latin phrase but english"
+      latinPhrase5: "wow the 5th latin phrase but english",
+      pokeId: ''
       
     }
   }
@@ -27,7 +28,7 @@ class App extends Component {
   // Connecting application to firebase database where I have objects stored that hold information that adds to the story
   componentDidMount() {
 
-    const randomPokemon = Math.floor(Math.random() * 487) + 1
+    const randomPokemon = Math.floor(Math.random() * 484) + 1
 
     console.log("this is the random pokemon number", randomPokemon)
     axios({
@@ -101,8 +102,8 @@ class App extends Component {
         latinPhrase1: randomLatinSentence1,
         latinPhrase2: "You're not sure because it's a deprecated language that isn't supported by Internet Explorer",
         latinPhrase3: "Embraced by ancient rhythms of the world, history pushes its way into the present",
-        
         latinPhrase4: "All the possibilities lay spread out before you are both ever-changing and ever-the-same",
+        pokeId: randomPokemon
       })
     })
     
@@ -122,23 +123,38 @@ class App extends Component {
         {/* Enlightenment story is told here, where information retrived from my firebase database will append */}
         
         <h1>~Trippy Tincture~</h1>
-        <h3>  
+        <header>
+          <h3>  
           Your friend, a holistic nutritionist, made you an herbal tincture meant to help reduce your stress levels. You decide to enjoy the clear weather by heading to your favourite park right after having a some drops of the tincture.
       </h3>
         <p className="para">
           {this.state.nightPhrase1} || {this.state.latinPhrase1}
         </p>
         
-        <Questions getStoryProp={this.getStory} />  
+        <Questions getStoryProp={this.getStory} /> 
+        </header>
+        
 
         <div className="pokeImage">
           IMAGE TO BE PLACED HEERE
-          <img src="" alt=""/>
+          
         </div>
-        
-        {/* <p className="dayStory">This is the daytime story. After taking some drops of tincture, heading to the park sounded like the perfect plan. You leave the dooming comfort of your home and take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase3}</span>. You relax on an empty bench in the middle of an empty park, tempted to test out the effects of the ticture. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase1}</span>. You close your eyes and feel the warmth of the sun gently looming over your face. You feel a heavy drowzines and notice that you feel more with your eyes closed while  <span className="daySpan">{this.state.dayPhrase2}</span>. You drift off into sleep. You hear a voice in the distance, <span className="latinSpan">"{this.state.latinPhrase1}"</span>. Was that latin? <span className="latinSpan">{this.state.latinPhrase2}</span>. You feel an alleviated sense of calmness washing over you as hear the voice in the distance getting closer to you echoing the harmony of the spheres floating up in outer space. "Each road taken holds challenges untold, which road you choose is how life unfolds. <span className="latinSpan">{this.state.latinPhrase3}</span>" echoed the voice in the vastless void of subconsciousness. Your senses have been heightened tenfold and you begin to see linear colour gradients zooming past your sides, eventuall displaying an ominous sphere of light directly infront of you. Before you know it, you extend your hand to reach for the floating orb and suddently make a profound realization that you are as limitless as chaos and nothingness. This epiphany brings about the release of all your axiety and worries that have plagued you all these months. "<span className="latinSpan">{this.state.latinPhrase4}</span>" was the last thing you heard from the orb before opening your eyes, with it, a wave of good fortune bids you adieu. You open up your eyes to <span className="nightSpan">{this.state.nightPhrase1}</span>.</p>
+        <img src={require("./assets/pokemon/"+{this.state.pokeId}+".svg")} alt="" />
 
-        <p className="nightStory">This is the night story. After taking some drops of tincture, heading to the park sounded like the perfect plan. You leave the dooming comfort of your home and take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase3}</span>. You relax on an empty bench in the middle of an empty park, tempted to test out the effects of the ticture. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase1}</span>. You close your eyes and feel the warmth of the sun gently looming over your face. You feel a heavy drowzines and notice that you feel more with your eyes closed while  <span className="daySpan">{this.state.dayPhrase2}</span>. You drift off into sleep. You hear a voice in the distance, <span className="latinSpan">"{this.state.latinPhrase1}"</span>. Was that latin? <span className="latinSpan">{this.state.latinPhrase2}</span>. You feel an alleviated sense of calmness washing over you as hear the voice in the distance getting closer to you echoing the harmony of the spheres floating up in outer space. "Each road taken holds challenges untold, which road you choose is how life unfolds. <span className="latinSpan">{this.state.latinPhrase3}</span>" echoed the voice in the vastless void of subconsciousness. Your senses have been heightened tenfold and you begin to see linear colour gradients zooming past your sides, eventuall displaying an ominous sphere of light directly infront of you. Before you know it, you extend your hand to reach for the floating orb and suddently make a profound realization that you are as limitless as chaos and nothingness. This epiphany brings about the release of all your axiety and worries that have plagued you all these months. "<span className="latinSpan">{this.state.latinPhrase4}</span>" was the last thing you heard from the orb before opening your eyes, with it, a wave of good fortune bids you adieu. You open up your eyes to <span className="nightSpan">{this.state.nightPhrase1}</span>.</p> */}
+        <section className="story">
+          <div className="dayDiv">
+            <p className="dayStory">This is the daytime story. After taking some drops of tincture, heading to the park sounded like the perfect plan. You leave the dooming comfort of your home and take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase3}</span>. You relax on an empty bench in the middle of an empty park, tempted to test out the effects of the ticture. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase1}</span>. You close your eyes and feel the warmth of the sun gently looming over your face. You feel a heavy drowzines and notice that you feel more with your eyes closed while  <span className="daySpan">{this.state.dayPhrase2}</span>. You drift off into sleep. You hear a voice in the distance, <span className="latinSpan">"{this.state.latinPhrase1}"</span>. Was that latin? <span className="latinSpan">{this.state.latinPhrase2}</span>. You feel an alleviated sense of calmness washing over you as hear the voice in the distance getting closer to you echoing the harmony of the spheres floating up in outer space. "Each road taken holds challenges untold, which road you choose is how life unfolds. <span className="latinSpan">{this.state.latinPhrase3}</span>" echoed the voice in the vastless void of subconsciousness. Your senses have been heightened tenfold and you begin to see linear colour gradients zooming past your sides, eventuall displaying an ominous sphere of light directly infront of you. Before you know it, you extend your hand to reach for the floating orb and suddently make a profound realization that you are as limitless as chaos and nothingness. This epiphany brings about the release of all your axiety and worries that have plagued you all these months. "<span className="latinSpan">{this.state.latinPhrase4}</span>" was the last thing you heard from the orb before opening your eyes, with it, a wave of good fortune bids you adieu. You open up your eyes to <span className="nightSpan">{this.state.nightPhrase1}</span>.</p>
+          </div>
+
+          <div className="nightDiv">
+            <p className="nightStory">This is the night story. After taking some drops of tincture, heading to the park sounded like the perfect plan. You leave the dooming comfort of your home and take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase3}</span>. You relax on an empty bench in the middle of an empty park, tempted to test out the effects of the ticture. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase1}</span>. You close your eyes and feel the warmth of the sun gently looming over your face. You feel a heavy drowzines and notice that you feel more with your eyes closed while  <span className="daySpan">{this.state.dayPhrase2}</span>. You drift off into sleep. You hear a voice in the distance, <span className="latinSpan">"{this.state.latinPhrase1}"</span>. Was that latin? <span className="latinSpan">{this.state.latinPhrase2}</span>. You feel an alleviated sense of calmness washing over you as hear the voice in the distance getting closer to you echoing the harmony of the spheres floating up in outer space. "Each road taken holds challenges untold, which road you choose is how life unfolds. <span className="latinSpan">{this.state.latinPhrase3}</span>" echoed the voice in the vastless void of subconsciousness. Your senses have been heightened tenfold and you begin to see linear colour gradients zooming past your sides, eventuall displaying an ominous sphere of light directly infront of you. Before you know it, you extend your hand to reach for the floating orb and suddently make a profound realization that you are as limitless as chaos and nothingness. This epiphany brings about the release of all your axiety and worries that have plagued you all these months. "<span className="latinSpan">{this.state.latinPhrase4}</span>" was the last thing you heard from the orb before opening your eyes, with it, a wave of good fortune bids you adieu. You open up your eyes to <span className="nightSpan">{this.state.nightPhrase1}</span>.</p>
+          </div>
+
+      </section>
+
+        
+
+        
 
       </div>
     
