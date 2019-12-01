a78c895400321e1388ad3d324c940abb44d9480d
Reem H <reemhamoui92@gmail.com>
2019-12-01 12:36:23 -0500

    added the pokemon API to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import firebase from './firebase'
-import Questions from './Questions'
-import './App.css';
+import React, { Component } from "react";
+import firebase from "./firebase"
+import axios from "axios"
+import Questions from "./Questions"
+import "./App.css";
 
 
 class App extends Component {
@@ -10,30 +11,51 @@ class App extends Component {
     this.state = {
       data: {},
       story: [],
-      dayPhrase1: '',
-      dayPhrase2: '',
-      nightPhrase1: '',
-      nightPhrase2: '',
-      latinPhrase1: '',
-      latinPhrase2: 'this is a placeholder in english',
-      latinPhrase3: 'another placeholder',
-      latinPhrase4: 'another another',
-      latinPhrase5: 'wow the 5th latin phrase but english'
+      dayPhrase1: "",
+      dayPhrase2: "",
+      nightPhrase1: "",
+      nightPhrase2: "",
+      latinPhrase1: "",
+      latinPhrase2: "this is a placeholder in english",
+      latinPhrase3: "another placeholder",
+      latinPhrase4: "another another",
+      latinPhrase5: "wow the 5th latin phrase but english"
       
     }
   }
 
   // Connecting application to firebase database where I have objects stored that hold information that adds to the story
   componentDidMount() {
+
+    const randomPokemon = Math.floor(Math.random() * 487) + 1
+
+    console.log("this is the random pokemon number", randomPokemon)
+    axios({
+      url: "https://pokeapi.co/api/v2/pokemon/" + randomPokemon,
+      method: "GET",
+      dataResponse: "json",
+    }).then((result) => {
+      console.log("the api result", result)
+      console.log("this is the pokemon name", result.data.name)
+      const pokeName = (result.data.name).charAt(0).toUpperCase() + (result.data.name).substr(1);
+      console.log("the capped name", pokeName)
+
+      const moveNumber = (Math.floor(Math.random(result.data.moves) * result.data.moves.length));
+      const moveName = (result.data.moves[moveNumber].move.name).charAt(0).toUpperCase() + (result.data.moves[moveNumber].move.name).substr(1);
+      console.log("this is the move name", moveName);
+    })
+
+
+    
+
+
     const dbRef = firebase.database().ref();
-    // console.log(dbRef)
-    dbRef.on('value', (snapshot) => {
+    
+    dbRef.on("value", (snapshot) => {
       // make a variable that would refer to our firebase data
       const storyData = snapshot.val()
 
       const strings = Object.values(storyData)
-      // console.log('this is the story data',strings)
-      // view the data we have in firebase on our console.log 
       const dayArray = strings[0];
       const dayArrayCopy = [...dayArray];
 
@@ -44,19 +66,19 @@ class App extends Component {
       const nightArrayCopy = [...nightArray];
       // making a copy of the day array 
       
-// create a function that randomizes multiple times over the selected phrases in the array and splices out whatever was already used during render
+      // create a function that randomizes multiple times over the selected phrases in the array and splices out whatever was already used during render
 
       // random day sentences 
       const randomDaySentence1 = dayArrayCopy.splice((Math.floor(Math.random(dayArrayCopy) * dayArrayCopy.length)), 1);
       const randomDaySentence2 = dayArrayCopy.splice((Math.floor(Math.random(dayArrayCopy) * dayArrayCopy.length)), 1);
       const randomDaySentence3 = dayArrayCopy.splice((Math.floor(Math.random(dayArrayCopy) * dayArrayCopy.length)), 1);
-      console.log('random day sentences',randomDaySentence1,randomDaySentence2)
+      console.log("random day sentences",randomDaySentence1,randomDaySentence2)
 
       // random night sentences
       const randomNightSentence1 = nightArrayCopy.splice((Math.floor(Math.random(nightArrayCopy) * nightArrayCopy.length)), 1)
       const randomNightSentence2 = nightArrayCopy.splice((Math.floor(Math.random(nightArrayCopy) * nightArrayCopy.length)), 1)
-      console.log('random night sentences', randomNightSentence1, randomNightSentence2);
-      // console.log('here is the random day index thingyy', randomDaySentence1, randomDayIndex);
+      console.log("random night sentences", randomNightSentence1, randomNightSentence2);
+      // console.log("here is the random day index thingyy", randomDaySentence1, randomDayIndex);
 
 
       // random latin sentences
@@ -66,7 +88,7 @@ class App extends Component {
       const randomLatinSentence4 = latinArrayCopy.splice((Math.floor(Math.random(latinArrayCopy) * latinArrayCopy.length)), 1)
       const randomLatinSentence5 = latinArrayCopy.splice((Math.floor(Math.random(latinArrayCopy) * latinArrayCopy.length)), 1)
 
-      console.log('THESE ARE ALL THE RANDOM LATIN SENTENCES', randomLatinSentence1, randomLatinSentence2, randomLatinSentence3, randomLatinSentence4, randomLatinSentence5)
+      console.log("THESE ARE ALL THE RANDOM LATIN SENTENCES", randomLatinSentence1, randomLatinSentence2, randomLatinSentence3, randomLatinSentence4, randomLatinSentence5)
 
       
       this.setState({
@@ -77,20 +99,19 @@ class App extends Component {
         nightPhrase1: randomNightSentence1,
         nightPhrase2: randomNightSentence2,
         latinPhrase1: randomLatinSentence1,
-        latinPhrase2: 'You\'re not sure because it\'s a deprecated language that isn\'t supported by Internet Explorer',
-        latinPhrase3: 'Embraced by ancient rhythms of the world, history pushes its way into the present',
+        latinPhrase2: "You're not sure because it's a deprecated language that isn't supported by Internet Explorer",
+        latinPhrase3: "Embraced by ancient rhythms of the world, history pushes its way into the present",
         
-        latinPhrase4: 'All the possibilities lay spread out before you are both ever-changing and ever-the-same',
+        latinPhrase4: "All the possibilities lay spread out before you are both ever-changing and ever-the-same",
       })
-
     })
     
   }
 
   getStory = (e, choice) => {
     e.preventDefault();
-    console.log('from app.js, this is being clicked', choice)
-    // console.log('hiiiiiiiiiiiiii',this.props.state)
+    console.log("from app.js, this is being clicked", choice)
+    
   }
 
   render() {
@@ -109,14 +130,20 @@ class App extends Component {
         </p>
         
         <Questions getStoryProp={this.getStory} />  
+
+        <div className="pokeImage">
+          IMAGE TO BE PLACED HEERE
+          <img src="" alt=""/>
+        </div>
         
-        <p className="dayStory">This is the daytime story. After taking some drops of tincture, heading to the park sounded like the perfect plan. You leave the dooming comfort of your home and take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase3}</span>. You relax on an empty bench in the middle of an empty park, tempted to test out the effects of the ticture. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase1}</span>. You close your eyes and feel the warmth of the sun gently looming over your face. You feel a heavy drowzines and notice that you feel more with your eyes closed while  <span className="daySpan">{this.state.dayPhrase2}</span>. You drift off into sleep. You hear a voice in the distance, <span className="latinSpan">"{this.state.latinPhrase1}"</span>. Was that latin? <span className="latinSpan">{this.state.latinPhrase2}</span>. You feel an alleviated sense of calmness washing over you as hear the voice in the distance getting closer to you echoing the harmony of the spheres floating up in outer space. "Each road taken holds challenges untold, which road you choose is how life unfolds. <span className="latinSpan">{this.state.latinPhrase3}</span>" echoed the voice in the vastless void of subconsciousness. Your senses have been heightened tenfold and you begin to see linear colour gradients zooming past your sides, eventuall displaying an ominous sphere of light directly infront of you. Before you know it, you extend your hand to reach for the floating orb and suddently make a profound realization that you are as limitless as chaos and nothingness. This epiphany brings about the release of all your axiety and worries that have plagued you all these months. "<span className="latinSpan">{this.state.latinPhrase4}</span>" was the last thing you heard from the orb before opening your eyes, with it, a wave of good fortune bids you adieu. You open up your eyes to <span className="nightSpan">{this.state.nightPhrase1}</span>.</p>
+        {/* <p className="dayStory">This is the daytime story. After taking some drops of tincture, heading to the park sounded like the perfect plan. You leave the dooming comfort of your home and take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase3}</span>. You relax on an empty bench in the middle of an empty park, tempted to test out the effects of the ticture. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase1}</span>. You close your eyes and feel the warmth of the sun gently looming over your face. You feel a heavy drowzines and notice that you feel more with your eyes closed while  <span className="daySpan">{this.state.dayPhrase2}</span>. You drift off into sleep. You hear a voice in the distance, <span className="latinSpan">"{this.state.latinPhrase1}"</span>. Was that latin? <span className="latinSpan">{this.state.latinPhrase2}</span>. You feel an alleviated sense of calmness washing over you as hear the voice in the distance getting closer to you echoing the harmony of the spheres floating up in outer space. "Each road taken holds challenges untold, which road you choose is how life unfolds. <span className="latinSpan">{this.state.latinPhrase3}</span>" echoed the voice in the vastless void of subconsciousness. Your senses have been heightened tenfold and you begin to see linear colour gradients zooming past your sides, eventuall displaying an ominous sphere of light directly infront of you. Before you know it, you extend your hand to reach for the floating orb and suddently make a profound realization that you are as limitless as chaos and nothingness. This epiphany brings about the release of all your axiety and worries that have plagued you all these months. "<span className="latinSpan">{this.state.latinPhrase4}</span>" was the last thing you heard from the orb before opening your eyes, with it, a wave of good fortune bids you adieu. You open up your eyes to <span className="nightSpan">{this.state.nightPhrase1}</span>.</p>
 
-        <p className="nightStory">This is the night story.  </p>
-
+        <p className="nightStory">This is the night story. After taking some drops of tincture, heading to the park sounded like the perfect plan. You leave the dooming comfort of your home and take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase3}</span>. You relax on an empty bench in the middle of an empty park, tempted to test out the effects of the ticture. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase1}</span>. You close your eyes and feel the warmth of the sun gently looming over your face. You feel a heavy drowzines and notice that you feel more with your eyes closed while  <span className="daySpan">{this.state.dayPhrase2}</span>. You drift off into sleep. You hear a voice in the distance, <span className="latinSpan">"{this.state.latinPhrase1}"</span>. Was that latin? <span className="latinSpan">{this.state.latinPhrase2}</span>. You feel an alleviated sense of calmness washing over you as hear the voice in the distance getting closer to you echoing the harmony of the spheres floating up in outer space. "Each road taken holds challenges untold, which road you choose is how life unfolds. <span className="latinSpan">{this.state.latinPhrase3}</span>" echoed the voice in the vastless void of subconsciousness. Your senses have been heightened tenfold and you begin to see linear colour gradients zooming past your sides, eventuall displaying an ominous sphere of light directly infront of you. Before you know it, you extend your hand to reach for the floating orb and suddently make a profound realization that you are as limitless as chaos and nothingness. This epiphany brings about the release of all your axiety and worries that have plagued you all these months. "<span className="latinSpan">{this.state.latinPhrase4}</span>" was the last thing you heard from the orb before opening your eyes, with it, a wave of good fortune bids you adieu. You open up your eyes to <span className="nightSpan">{this.state.nightPhrase1}</span>.</p> */}
 
       </div>
+    
     );
+    
   }
 }
 
