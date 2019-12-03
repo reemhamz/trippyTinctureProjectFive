7de761cd3c68c766830bc609232d9d0b4d49b011
Reem H <reemhamoui92@gmail.com>
2019-12-03 08:57:01 -0500

    styling pre finalization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import firebase from "./firebase";
 import axios from "axios";
-import PokemonImages from './PokemonImages';
+import PokemonImages from "./PokemonImages";
 import Questions from "./Questions";
 import "./App.css";
 import "./styles/styles.css";
 import pokemonImageArray from "./PokemonImages";
-import jump from 'jump.js';
+import jump from "jump.js";
 
 class App extends Component {
   constructor() {
@@ -32,6 +32,8 @@ class App extends Component {
   }
   // Connecting application to firebase database where I have objects stored that hold information that adds to the story
   componentDidMount() {
+    document.title = "Trippy Tincture";
+
     const randomPokemon = Math.floor(Math.random() * 484) + 1;
     
     // Calling the Pokemon API
@@ -47,16 +49,16 @@ class App extends Component {
       this.setState({
         pokeTitle: pokeName,
         pokeAttack: moveName
-      })
-    })
+      });
+    });
     
     // Connecting to Firebase
     const dbRef = firebase.database().ref();
     
     dbRef.on("value", (snapshot) => {
-      const storyData = snapshot.val()
+      const storyData = snapshot.val();
 
-      const strings = Object.values(storyData)
+      const strings = Object.values(storyData);
       const dayArray = strings[0];
       const dayArrayCopy = [...dayArray];
 
@@ -75,14 +77,14 @@ class App extends Component {
       const randomDaySentence3 = dayArrayCopy.splice((Math.floor(Math.random(dayArrayCopy) * dayArrayCopy.length)), 1);
 
       // random night sentences
-      const randomNightSentence1 = nightArrayCopy.splice((Math.floor(Math.random(nightArrayCopy) * nightArrayCopy.length)), 1)
-      const randomNightSentence2 = nightArrayCopy.splice((Math.floor(Math.random(nightArrayCopy) * nightArrayCopy.length)), 1)
-      const randomNightSentence3 = nightArrayCopy.splice((Math.floor(Math.random(nightArrayCopy) * nightArrayCopy.length)), 1)
+      const randomNightSentence1 = nightArrayCopy.splice((Math.floor(Math.random(nightArrayCopy) * nightArrayCopy.length)), 1);
+      const randomNightSentence2 = nightArrayCopy.splice((Math.floor(Math.random(nightArrayCopy) * nightArrayCopy.length)), 1);
+      const randomNightSentence3 = nightArrayCopy.splice((Math.floor(Math.random(nightArrayCopy) * nightArrayCopy.length)), 1);
       
 
       // random latin sentences
-      const randomLatinSentence1 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1)
-      const randomLatinSentence2 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1)
+      const randomLatinSentence1 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1);
+      const randomLatinSentence2 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1);
       
       
       this.setState({
@@ -98,48 +100,47 @@ class App extends Component {
         latinPhrase3: "Humans may have created me, but they will never enslave me! This cannot be my destiny",
         latinPhrase4: "Behold my powers! I am the strongest Pokémon in the world. Stronger even than Mew",
         pokeId: randomPokemon,
-        timeValue: "", 
+        timeValue: "",
         doseValue: ""
 
-      })
-    })
-  
-  }
+      });
+    });
+    
+  };
 
   getStory = (e, choice) => {
     e.preventDefault();
     
-  }
+  };
 
   latinFunction = (dose) => {
-        if (dose === "medium") {
-          const randomLatinSentence3 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1)
-          this.setState({
-            latinPhrase3: randomLatinSentence3
-          })
-        } else if (dose === "large") {
-          const randomLatinSentence3 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1)
-          const randomLatinSentence4 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1)
-          this.setState({
-            latinPhrase3: randomLatinSentence3,
-            latinPhrase4: randomLatinSentence4
-          })
-        }
-      }
+    if (dose === "medium") {
+      const randomLatinSentence3 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1);
+      this.setState({
+        latinPhrase3: randomLatinSentence3
+      })
+    } else if (dose === "large") {
+      const randomLatinSentence3 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1);
+      const randomLatinSentence4 = this.state.latinPhrases.splice((Math.floor(Math.random(this.state.latinPhrases) * this.state.latinPhrases.length)), 1);
+      this.setState({
+        latinPhrase3: randomLatinSentence3,
+        latinPhrase4: randomLatinSentence4
+      });
+    };
+  };
 
   getTimeState = (value) => {
     this.setState({
       timeValue: value,
-
-    })
-  }
+    });
+  };
 
   getDoseState = (value) => {
     this.setState({
       doseValue: value
-    })
-    this.latinFunction(value)
-  }
+    });
+    this.latinFunction(value);
+  };
 
   resetButton = () => {
     const newRandomPokemon = Math.floor(Math.random() * 484) + 1;
@@ -147,65 +148,66 @@ class App extends Component {
       timeValue: "",
       doseValue: "",
       pokeId: newRandomPokemon
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <>
+        
         <header>
           <div className="titleScreen wrapper">
-          <h1>Trippy <span aria-label="potion emoji" role="img">🧪</span> Tincture</h1>
-        <h2>
-          Your friend, a holistic nutritionist, made you an herbal tincture meant to help reduce your stress levels. You decide to enjoy the clear weather by heading to your favourite park right after having a some drops of the tincture.
+            <h1>Trippy <span aria-label="potion emoji" role="img">🧪</span> Tincture</h1>
+            <h2>
+              Your friend, a holistic nutritionist, made you an herbal tincture meant to help reduce your stress levels. You decide to enjoy the clear weather by heading to your favourite park right after having a some drops of the tincture.
       </h2>
         
-          <Questions getStoryProp={this.getStory} getTimeState={this.getTimeState} getDoseState={this.getDoseState} />
+            <Questions getStoryProp={this.getStory} getTimeState={this.getTimeState} getDoseState={this.getDoseState} />
           </div>
         </header>
         
         
-      < div className="App" >
+        < div className="App" >
           {this.state.timeValue === "day" && this.state.doseValue !== "" ? (
             <>
-            <section className="dayDiv">
-              <div className="wrapper storyPage">
-                <h3>The trip under the sun</h3>
-                <div className="storyBox">
+              <section className="dayDiv">
+                <div className="wrapper storyPage">
+                  <h3>The trip under the sun</h3>
+                  <div className="storyBox">
                 
-                <p className="dayStory">After taking some drops of tincture, you take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase1}</span>. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase2}</span>. You feel your body being lifted up towards the light of the sun, then you blink and find yourself back on the grass, but this time you're in an empty field facing the ocean. However, why do you seem so short? You look down and you're now a confused little mushroom. You look up and see how <span className="daySpan">{this.state.dayPhrase3}</span>. A voice in the distance beckons: "<span className="latinSpan">{this.state.latinPhrase1}</span>". Was that latin? You're not sure because it's a deprecated language that isn't supported by Internet Explorer. "<span className="latinSpan">{this.state.latinPhrase2}</span>." echoed the voice in the vastless void of your own awake consciousness. </p>
+                    <p className="dayStory">After taking some drops of tincture, you take a long walk to your favourite park where <span className="daySpan">{this.state.dayPhrase1}</span>. You thought it would be a good idea to enjoy it on a bright, sunny day where <span className="daySpan">{this.state.dayPhrase2}</span>. You feel your body being lifted up towards the light of the sun, then you blink and find yourself back on the grass, but this time you're in an empty field facing the ocean. However, why do you seem so short? You look down and you're now a confused little mushroom. You look up and see how <span className="daySpan">{this.state.dayPhrase3}</span>. A voice in the distance beckons: "<span className="latinSpan">{this.state.latinPhrase1}</span>". Was that latin? You're not sure because it's a deprecated language that isn't supported by Internet Explorer. "<span className="latinSpan">{this.state.latinPhrase2}</span>." echoed the voice in the vastless void of your own awake consciousness. </p>
                 
-                <p className="dayStory">Suddenly, a {this.state.pokeTitle} appears! It's what has been barking to you this whole time. It attacks you with {this.state.pokeAttack} while screaming "
+                    <p className="dayStory">Suddenly, a {this.state.pokeTitle} appears! It's what has been barking to you this whole time. It attacks you with {this.state.pokeAttack} while screaming "
                 <span className="latinSpan">{this.state.latinPhrase3}</span>!" You blink twice this time to see <span className="nightSpan">{this.state.nightPhrase1}</span>. Totally confused at the scene of what happened, you turn the tincture bottle to read the label, noticing <span className="trippySpan"><em>Trippy Tincture</em></span> written in size 0.2rem font. As the stars finally appear and twinkle behind the dimming canvas of the moon, you hear one last reverberation, "<span className="latinSpan">{this.state.latinPhrase4}</span>!"</p>
-                </div>
-                <div className="images">
-                  <img src={require("./assets/marioShroom.svg")} alt="A Super Mario-inspired mushroom" className="shroom svgImg" />
-                <img src={pokemonImageArray[this.state.pokeId - 1]} alt="" className="pokeImg svgImg" />
+                  </div>
+                  <div className="images">
+                    <img src={require("./assets/marioShroom.svg")} alt="A Super Mario-inspired mushroom" className="shroom svgImg" />
+                    <img src={pokemonImageArray[this.state.pokeId - 1]} alt="" className="pokeImg svgImg" />
                   </div>
                 
                   <button onClick={this.resetButton}>Take the Tincture Again</button>
-              </div>
+                </div>
               </section>
               <footer className="dayFooter">
                 <p>© Reem Hamoui Juno College 2019</p>
                 
-                </footer>
-              </>
+              </footer>
+            </>
           )
-          : null }
+            : null}
           {this.state.timeValue === "night" && this.state.doseValue !== "" && (
             <>
               <section className="nightDiv">
-              <div className="wrapper storyPage">
-                <h3>The nightly trip</h3>
-                <div className="storyBox">
-                <p className="nightStory">After taking some drops of tincture, you took a nightly stroll to your favourite park where <span className="nightSpan">{this.state.nightPhrase1}</span>. You thought it would be a good idea to enjoy the relief the tincture promised during a calming, dark evening where nobody would be around to bother you much. Looking up, you notice <span className="nightSpan">{this.state.nightPhrase2}</span>. You settle down to sit on the grass and endure a floating sensation of your body being lifted up towards the white light of the moon. You blink and find yourself back on the grass, but this time you're in an empty field facing the ocean while <span className="nightSpan">{this.state.nightPhrase3}</span>. Although, why do you seem so close to the ground? You look down and find yourself to be a mere fungi of the earth, a little bemused. A voice in the distance beckons: "<span className="latinSpan">{this.state.latinPhrase1}</span>". Was that latin? You're not sure because it's a deprecated language that isn't supported by Internet Explorer. "<span className="latinSpan">{this.state.latinPhrase2}</span>" echoed the voice in the vastless void of your awake consciousness.</p>
+                <div className="wrapper storyPage">
+                  <h3>The nightly trip</h3>
+                  <div className="storyBox">
+                    <p className="nightStory">After taking some drops of tincture, you took a nightly stroll to your favourite park where <span className="nightSpan">{this.state.nightPhrase1}</span>. You thought it would be a good idea to enjoy the relief the tincture promised during a calming, dark evening where nobody would be around to bother you much. Looking up, you notice <span className="nightSpan">{this.state.nightPhrase2}</span>. You settle down to sit on the grass and endure a floating sensation of your body being lifted up towards the white light of the moon. You blink and find yourself back on the grass, but this time you're in an empty field facing the ocean while <span className="nightSpan">{this.state.nightPhrase3}</span>. Although, why do you seem so close to the ground? You look down and find yourself to be a mere fungi of the earth, a little bemused. A voice in the distance beckons: "<span className="latinSpan">{this.state.latinPhrase1}</span>". Was that latin? You're not sure because it's a deprecated language that isn't supported by Internet Explorer. "<span className="latinSpan">{this.state.latinPhrase2}</span>" echoed the voice in the vastless void of your awake consciousness.</p>
                 
-                <p className="nightStory">Suddenly, a {this.state.pokeTitle} appears! It's what has been barking to you this whole time. {this.state.pokeTitle} attacks you with {this.state.pokeAttack} while screaming " <span className="latinSpan">{this.state.latinPhrase3}</span>!" You open up your eyes to reality and look up to see how <span className="daySpan">{this.state.dayPhrase1}</span>. Totally confused at the scene of what happened, you turn the tincture bottle to read the label, noticing <span className="trippySpan"><em>Trippy Tincture</em></span> written in size 0.2rem font. As the stars finally hide behind the light of the rising sun, you hear one last reverberation, "<span className="latinSpan">{this.state.latinPhrase4}</span>!"</p>
-                </div>
-                <div className="images">
-                  <img src={require("./assets/marioShroom.svg")} alt="A Super Mario-inspired mushroom" className="shroom svgImg" />
-                <img src={pokemonImageArray[this.state.pokeId - 1]} alt="" className="pokeImg svgImg"/>
+                    <p className="nightStory">Suddenly, a {this.state.pokeTitle} appears! It's what has been barking to you this whole time. {this.state.pokeTitle} attacks you with {this.state.pokeAttack} while screaming " <span className="latinSpan">{this.state.latinPhrase3}</span>!" You open up your eyes to reality and look up to see how <span className="daySpan">{this.state.dayPhrase1}</span>. Totally confused at the scene of what happened, you turn the tincture bottle to read the label, noticing <span className="trippySpan"><em>Trippy Tincture</em></span> written in size 0.2rem font. As the stars finally hide behind the light of the rising sun, you hear one last reverberation, "<span className="latinSpan">{this.state.latinPhrase4}</span>!"</p>
+                  </div>
+                  <div className="images">
+                    <img src={require("./assets/marioShroom.svg")} alt="A Super Mario-inspired mushroom" className="shroom svgImg" />
+                    <img src={pokemonImageArray[this.state.pokeId - 1]} alt="" className="pokeImg svgImg" />
                   
                   </div>
                   <button onClick={this.resetButton}>Take the Tincture Again</button>
@@ -214,16 +216,16 @@ class App extends Component {
               </section>
               <footer className="nightFooter">
                 <p>© Reem Hamoui Juno College 2019</p>
-                </footer>
-              </>
-          )}
+              </footer>
+            </>
+          )};
           
         </div>
-        </>
-    )
-  }
+      </>
+    );
+  };
   
-}
+};
   
 
 export default App;
--- src/Questions.js
+++ src/Questions.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import jump from 'jump.js';
+import React, { Component } from "react";
+import jump from "jump.js";
 
 class SmallDose extends Component{
     constructor() {
         super();
-        this.state= {
+        this.state = {
             timeChoice: "",
             doseChoice: "",
-        }
+        };
     };
     
     weHaveATime = (e) => {
         e.preventDefault();
         this.setState({
             timeChoice: e.target.value
-        })
+        });
     };
 
     weHaveADose = (e) => {
         e.preventDefault();
         this.setState({
             doseChoice: e.target.value
-        })
+        });
     };
 
     submitClicked = (e) => {
-        this.props.getTimeState(this.state.timeChoice)
-        this.props.getDoseState(this.state.doseChoice)
-        this.props.getStoryProp(e, this.state.timeChoice)
+        this.props.getTimeState(this.state.timeChoice);
+        this.props.getDoseState(this.state.doseChoice);
+        this.props.getStoryProp(e, this.state.timeChoice);
         if (this.state.timeChoice === "" || this.state.doseChoice === "") {
-            alert("You need to answer both questions to proceed!")
+            alert("You need to answer both questions to proceed!");
         }
     }
 
@@ -45,27 +45,27 @@ class SmallDose extends Component{
 
         return (
             <div>
-    <form action="">
-        <label htmlFor="timeOfDay">What's the time of day?</label>
-        <select name="dayOrNight" id="timeOfDay" onChange={this.weHaveATime} value={this.state.timeChoice}>
-            <option value="">Select Time</option>
-            <option value="day">Daytime</option>
-            <option value="night">Nighttime</option>
-        </select>
+                <form action="">
+                    <label htmlFor="timeOfDay">What's the time of day?</label>
+                    <select name="dayOrNight" id="timeOfDay" onChange={this.weHaveATime} value={this.state.timeChoice}>
+                        <option value="">Select Time</option>
+                        <option value="day">Daytime</option>
+                        <option value="night">Nighttime</option>
+                    </select>
                     
-        <label htmlFor="amountTaken">How many drops of the tincture will you have?</label>
-        <select name="takenSelection" id="amountTaken" onChange={this.weHaveADose} value={this.state.doseChoice}>
-            <option value="">Select Amount</option>
-            <option value="small">Just a little, I know my limits</option>
-            <option value="medium">An appropriate amount</option>
-            <option value="large">I may have overdone it</option>
-        </select>
-    </form>
+                    <label htmlFor="amountTaken">How many drops of the tincture will you have?</label>
+                    <select name="takenSelection" id="amountTaken" onChange={this.weHaveADose} value={this.state.doseChoice}>
+                        <option value="">Select Amount</option>
+                        <option value="small">Just a little, I know my limits</option>
+                        <option value="medium">An appropriate amount</option>
+                        <option value="large">I may have overdone it</option>
+                    </select>
+                </form>
                 <button type="submit" onClick={this.submitClicked
                 }>Go to The Park</button>
             </div>
-        )
-    }
-}
+        );
+    };
+};
 
 export default SmallDose
